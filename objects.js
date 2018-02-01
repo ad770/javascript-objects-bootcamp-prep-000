472ed1807a417bef9f0c72bfc78ab607dee789df
arye dov eidelman <aryedov.e@gmail.com>
2018-02-01 19:42:39 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,8 @@
-playlist = {};
+playlist = { Kanye: "Gold Digger" };
+
+function updatePlaylist(playlist, artistName, songTitle){
+  return OBJECT.ASSIGN({}, playlist, {artistName: songTitle});
+}
+function removeFromPlaylist(playlist, artistName){
+  
+}
